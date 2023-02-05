c34c49238d8d943b2fbed70038c23c9b71b9b5b5
Souharda Biswas <mail2souharda2015@gmail.com>
2023-02-05 17:54:32 +0530

    Integrated DB

--- api/index.js
+++ api/index.js
@@ -1,15 +1,29 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
-app.use(cors);
-app.use(express.json());
+const cors = require("cors");
+const { mongo, default: mongoose } = require("mongoose");
+const User = require("./models/User");
 
-app.get("/register", (req, res) => {
+app.use(express.json());
+app.use(cors());
+mongoose.set("strictQuery", false);
+
+mongoose.connect(
+  "mongodb+srv://blog:dBBw4P7hTiYaWxz3@cluster0.8smfzlq.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.get("/test", (req, res) => {
+  res.json("test 0k");
+});
+
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  res.json({ requestData: { username, password } });
-  res.json("test ok");
-  console.log(requestData);
+  try {
+    const userDoc = await User.create({ username, password });
+    res.json(userDoc);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 app.listen(4000);
